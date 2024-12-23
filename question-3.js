329220163c91f819fd641ca3458c80f80446ed08
Sappakit <pipitwattana.s@gmail.com>
2024-12-23 10:46:42 +0700

    feat: complete all tests

--- question-3.js
+++ question-3.js
@@ -1,20 +1,25 @@
 /*
 What is the Big O notation for Function findStudentById . Explain your reason.
-Answer:
+Answer: O(n)
+        โดยดูจากกระบวนการทำงานที่มี Time complexity สูงสุด 1 ตัว คือ Linear search ซึ่งมีค่าเท่ากับ O(n)
 
 
 */
 
 /* 
 What is the Big O notation for Function findProductPrice. Explain your reason.
-Answer:
+Answer: O(log n)
+        โดยดูจากกระบวนการทำงานที่มี Time complexity สูงสุด 1 ตัว คือ Binary search ซึ่งมีค่าเท่ากับ O(log n)
 
 
 */
 
 /* 
 Which function is more efficient and why?
-Answer:
-
-
+Answer: ฟังก์ชัน findProductPrice จะมีประสิทธิภาพมากกว่า
+        เนื่องจาก Binary search เป็นทำงานโดยการแบ่งครึ่ง Array ไปเรื่อย ๆ
+        ทำให้จำนวนครั้งในการตรวจสอบน้อยกว่า จากการที่ไม่ต้องตรวจสอบสมาชิกทุกตัว
+        ถึงแม้จำนวนของข้อมูลจะมากขึ้น จำนวนรอบการทำงานก็จะไม่ได้เพิ่มขึ้นมากนัก
+        ซึ่งต่างจาก Linear search ที่จะต้องมีการตรวจสอบสมาชิกทุกตัวเรียงลำดับไปเรื่อย ๆ
+        ยิ่งข้อมูลมาก ยิ่งมีจำนวนรอบการทำงานมาก
 */
--- question-5.js
+++ question-5.js
@@ -1,6 +1,6 @@
 /*  
 What is the Big O notation for Function sortProductsByPrice of the Question 4. Explain your reason.
-Answer:
-
+Answer: O(n^2)
+        โดยดูจากกระบวนการทำงานที่มี Time complexity สูงสุด 1 ตัว คือ Bubble sort (Loop ซ้อน Loop) ซึ่งมีค่าเท่ากับ O(n^2)
 
 */
